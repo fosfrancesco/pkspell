642e170f72c722c13a17e6e22ab7fc0f853c5a90
Francesco FOSCARIN <foscarif@kali3.cnam.fr>
2021-03-25 17:50:11 +0100

    normalized validation accuracy for each piece during train

--- train.py
+++ train.py
@@ -32,7 +32,8 @@ def training_loop(
     for i_epoch in range(1, epochs + 1):
         t0 = time.time()
         loss_sum = 0
-        accuracy_sum = 0
+        accuracy_pitch_sum = 0
+        accuracy_ks_sum = 0
         model.train()
         for idx, (seqs, pitches, keysignatures, lens,) in enumerate(
             train_dataloader
@@ -53,44 +54,46 @@ def training_loop(
 
             with torch.no_grad():
                 predicted_pitch, predicted_ks = model.predict(seqs, lens)
-                for i, p in enumerate(predicted_pitch):
+                for i, (p, k) in enumerate(zip(predicted_pitch, predicted_ks)):
                     # compute the accuracy without considering the padding
-                    acc = accuracy_score(p, pitches[:, i][: len(p)].cpu())
+                    acc_pitch = accuracy_score(p, pitches[:, i][: len(p)].cpu())
+                    acc_ks = accuracy_score(k, keysignatures[:, i][: len(k)].cpu())
                     # normalize according to the number of sequences in the batch
-                    accuracy_sum += acc / len(lens)
+                    accuracy_pitch_sum += acc_pitch / len(lens)
+                    accuracy_ks_sum += acc_ks / len(lens)
 
         train_loss = loss_sum / len(train_dataloader)
         # normalize according to the number of batches
-        train_accuracy = accuracy_sum / len(train_dataloader)
+        train_accuracy_pitch = accuracy_pitch_sum / len(train_dataloader)
+        train_accuracy_ks = accuracy_ks_sum / len(train_dataloader)
         history["train_loss"].append(train_loss)
-        history["train_accuracy"].append(train_accuracy)
-        writer.add_scalar("Accuracy_Pitch/train", train_accuracy, i_epoch)
+        history["train_accuracy_pitch"].append(train_accuracy_pitch)
+        history["train_accuracy_ks"].append(train_accuracy_ks)
+        writer.add_scalar("Accuracy_Pitch/train", train_accuracy_pitch, i_epoch)
+        writer.add_scalar("Accuracy_KS/train", train_accuracy_ks, i_epoch)
 
         if val_dataloader is not None:
             # Evaluate on the validation set
             model.eval()
-            all_predicted_pitch = []
-            all_predicted_ks = []
-            all_pitches = []
-            all_ks = []
+            accuracy_pitch_sum = 0
+            accuracy_ks_sum = 0
             with torch.no_grad():
                 for seqs, pitches, keysignatures, lens in val_dataloader:
                     # Predict the model's output on a batch
                     predicted_pitch, predicted_ks = model.predict(seqs.to(device), lens)
                     # Update the lists that will be used to compute the accuracy
                     for i, (p, k) in enumerate(zip(predicted_pitch, predicted_ks)):
-                        all_predicted_pitch.append(torch.Tensor(p))
-                        all_predicted_ks.append(torch.Tensor(k))
-                        all_pitches.append(pitches[0 : int(lens[i]), i])
-                        all_ks.append(keysignatures[0 : int(lens[i]), i])
+                        # compute the accuracy without considering the padding
+                        acc_pitch = accuracy_score(p, pitches[:, i][: len(p)].cpu())
+                        acc_ks = accuracy_score(k, keysignatures[:, i][: len(k)].cpu())
+                        # normalize according to the number of sequences in the batch
+                        accuracy_pitch_sum += acc_pitch / len(lens)
+                        accuracy_ks_sum += acc_ks / len(lens)
+                
+                # normalize according to the number of batches
+                val_accuracy_pitch = accuracy_pitch_sum / len(val_dataloader)
+                val_accuracy_ks = accuracy_ks_sum / len(val_dataloader)
 
-            # Compute the overall accuracy for the validation set
-            val_accuracy_pitch = accuracy_score(
-                torch.cat(all_predicted_pitch), torch.cat(all_pitches)
-            )
-            val_accuracy_ks = accuracy_score(
-                torch.cat(all_predicted_ks), torch.cat(all_ks)
-            )
             history["val_accuracy_pitch"].append(val_accuracy_pitch)
             history["val_accuracy_ks"].append(val_accuracy_ks)
 
@@ -107,6 +110,6 @@ def training_loop(
 
         t1 = time.time()
         print(
-            f"Epoch {i_epoch}: train loss = {train_loss:.4f}, train_accuracy: {train_accuracy:.4f},val_accuracy_pitch: {val_accuracy_pitch:.4f},val_accuracy_ks: {val_accuracy_ks:.4f}, time = {t1-t0:.4f}"
+            f"Epoch {i_epoch}: train loss = {train_loss:.4f}, train_accuracy_pitch: {train_accuracy_pitch:.4f}, train_accuracy_ks: {train_accuracy_ks:.4f},val_accuracy_pitch: {val_accuracy_pitch:.4f},val_accuracy_ks: {val_accuracy_ks:.4f}, time = {t1-t0:.4f}"
         )
     return history
